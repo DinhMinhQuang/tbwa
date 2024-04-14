d233a43074319bda7607fa08c0288cf24e242f95
Minh Quang <letuyettrinh10c12@gmail.com>
2024-04-15 01:04:49 +0700

    cookie-notice.php

---
+++ cookie-notice.php
@@ -0,0 +1,12 @@
+<div id="js_cookieNotice" class="cookieNotice">
+    <div class="cookieNotice__container">
+        <p>
+            This website uses cookies to improve your experience.
+        </p>
+        <p>
+            <a id="js_cookieNotice_accept" href="#">Accept</a>
+            <a href="/wordpress/privacy-policy">Privacy Policy</a>
+        </p>
+
+    </div>
+</div>
